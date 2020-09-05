8797e878acfd283d29e79e912ea01da4c69c27a5
Ali Madooei <madooei@jhu.edu>
2020-09-05 13:48:25 -0400

    Add event listener for zzz button

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ root.append(p);
 
 let zzz = document.createElement("button");
 zzz.innerText = "zzz";
+zzz.addEventListener("click", zzzOnClick);
 root.append(zzz);
 
 let output = document.createElement("div");
@@ -17,3 +18,34 @@ output.innerHTML = `
 <p>If you head to bed right now, you should try to wake up at one of the following times:</p>
 <p id="hours">11:44 PM or 1:14 AM or 2:44 AM or 4:14 AM or 5:44 AM or 7:14 AM</p>
 <p>A good night's sleep consists of 5-6 complete sleep cycles.</p>`;
+
+
+function zzzOnClick() {
+    let output = document.querySelector(".output");
+    output.style.display = "block";
+    let hours = "";
+
+    // get current time
+    let now = Date.now(); // in milliseconds
+    let minute = 60 * 1000;  // milliseconds
+    let cycle = now;
+
+    // allow 14 minutes to fall sleep
+    cycle += 14 * minute;
+
+    // calculate 6 sleep cycles (each 90 minutes)
+    for(let i = 0; i < 6; i++) {
+        cycle += 90 * minute;
+
+        // append the sleep cycles to hours string
+        hours += new Date(cycle).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+        if (i < 5) {
+            hours += " OR ";
+        }
+    }
+
+    // output hours
+    let hoursElm = document.querySelector("#hours");
+    hoursElm.innerText = hours;
+}
